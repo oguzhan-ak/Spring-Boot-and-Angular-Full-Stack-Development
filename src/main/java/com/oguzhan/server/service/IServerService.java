e555947c0e19901fa96a2ab47d8a305ca1b60804
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2023-09-23 16:51:20 +0300

    creat eand ping service methods written

--- src/main/java/com/oguzhan/server/service/IServerService.java
+++ src/main/java/com/oguzhan/server/service/IServerService.java
@@ -2,11 +2,12 @@ package com.oguzhan.server.service;
 
 import com.oguzhan.server.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface IServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/com/oguzhan/server/service/ServerService.java
+++ src/main/java/com/oguzhan/server/service/ServerService.java
@@ -1,19 +1,39 @@
 package com.oguzhan.server.service;
 
+import com.oguzhan.server.enumaration.Status;
 import com.oguzhan.server.model.Server;
+import com.oguzhan.server.repo.IServerRepo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerService implements  IServerService{
+
+    private final IServerRepo serverRepo;
 
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        return serverRepo.save(server);
     }
 
     @Override
@@ -33,6 +53,10 @@ public class ServerService implements  IServerService{
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
